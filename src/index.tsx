49b83c4b768017ffc6b3c2db88626a8153a423c8
bugarcic <baronvie@gmail.com>
2021-12-27 17:03:55 -0500

    boilerplate webpack setup

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+// import path from 'path/posix';
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App'
+
+
+
+render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById('app'),
+)

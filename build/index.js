1850f4ff35c63e67b581d0efe1f17717951d6265
bugarcic <baronvie@gmail.com>
2021-12-28 12:30:45 -0500

    basic CRUD functionality with MongoDB

--- build/index.js
+++ build/index.js
@@ -106,7 +106,7 @@ eval("\n\nif (false) {} else {\n  module.exports = __webpack_require__(/*! ./cjs
   \*********************/
 /***/ (function(__unused_webpack_module, exports, __webpack_require__) {
 
-eval("\nvar __importDefault = (this && this.__importDefault) || function (mod) {\n    return (mod && mod.__esModule) ? mod : { \"default\": mod };\n};\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\n// import path from 'path/posix';\nconst react_1 = __importDefault(__webpack_require__(/*! react */ \"./node_modules/react/index.js\"));\nconst App = () => {\n    return (react_1.default.createElement(\"p\", null, \"hello\"));\n};\nexports[\"default\"] = App;\n\n\n//# sourceURL=webpack://heap-viz/./src/App.tsx?");
+eval("\nvar __importDefault = (this && this.__importDefault) || function (mod) {\n    return (mod && mod.__esModule) ? mod : { \"default\": mod };\n};\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\n// import path from 'path/posix';\nconst react_1 = __importDefault(__webpack_require__(/*! react */ \"./node_modules/react/index.js\"));\nconst App = () => {\n    return (react_1.default.createElement(\"p\", null, \"goodbye\"));\n};\nexports[\"default\"] = App;\n\n\n//# sourceURL=webpack://heap-viz/./src/App.tsx?");
 
 /***/ }),
 

2b5e5945c848c11669ab7b9a35f817f69629de97
Yogi Paturu <yogipaturu@Yogis-MacBook-Air.local>
2021-12-28 09:44:30 -0600

    new componenets

---
+++ src/Tree.tsx
@@ -0,0 +1,14 @@
+// import path from 'path/posix';
+import React from 'react';
+import { render } from 'react-dom';
+
+
+const App = () => {
+
+return (
+    <p>goodbye</p>
+    )
+}
+
+
+export default App;

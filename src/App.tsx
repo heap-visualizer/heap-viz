25769c5ec97b2dd2e7a356166c6192307de3cb2f
yingliufilms <yingliufilms@gmail.com>
2021-12-29 10:26:59 -0500

    stash style changes prior to pull from dev

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,22 @@
 import React from 'react';
 import { render } from 'react-dom';
 import LandingPage from './containers/LandingPage';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HeapViz from './containers/HeapViz';
 
+import './stylesheets/styles.scss';
 
 const App = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route
-                    path ={'/'}
-                    element={<LandingPage />}
-                />
-                <Route
-                    path ={'/main'}
-                    element={<HeapViz />}
-                />
-            </Routes>
-        </Router>
-    )
-}
+  return (
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route path={'/'} element={<LandingPage />} />
+          <Route path={'/main'} element={<HeapViz />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
--- src/components/HeapTree.tsx
+++ src/components/HeapTree.tsx
@@ -8,24 +8,35 @@ function nodesAndEdges(inputHeap: Heap): ElementDefinition[] {
   const { heap } = inputHeap;
   const middle = Math.floor(heap.length / 2) * 50;
   const range: number[] = [middle];
-  const firstNode = { data: { id: (0).toString(), label: `Val: ${heap[0]}` }, position: { x: middle, y: 0 } }
+  const firstNode = {
+    data: { id: (0).toString(), label: `Val: ${heap[0]}` },
+    position: { x: middle, y: 0 },
+  };
   const nodes: ElementDefinition[] = [firstNode];
   const edges: ElementDefinition[] = [];
   for (let i = 1; i < heap.length; i++) {
-    const shift = ((i + 1) % 2) ? 1 : -1;
-    const parent = Math.floor((i - 1) / 2)
+    const shift = (i + 1) % 2 ? 1 : -1;
+    const parent = Math.floor((i - 1) / 2);
     const parentNode = nodes[parent];
     const { position } = parentNode;
     const xpos = position.x + shift * range[parent];
     range.push(range[parent] / 2);
-    const currNode = { data: { id: (i).toString(), label: `Val: ${heap[i]}` }, position: { x: xpos, y: position.y + 100 } }
+    const currNode = {
+      data: { id: i.toString(), label: `Val: ${heap[i]}` },
+      position: { x: xpos, y: position.y + 100 },
+    };
     nodes.push(currNode);
 
-    edges.push({ data: { source: `${parent}`, target: `${i}`, label: `Edge from ${parent} to ${i}` } })
+    edges.push({
+      data: {
+        source: `${parent}`,
+        target: `${i}`,
+        label: `Edge from ${parent} to ${i}`,
+      },
+    });
   }
   return nodes.concat(edges);
 }
-
 
 const HeapTree = (array: number[] = []) => {
   const heap = new MinHeap(array);
@@ -37,14 +48,22 @@ const HeapTree = (array: number[] = []) => {
   const elements2 = [
     { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
     { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
-    { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
+    {
+      data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' },
+    },
   ];
 
   console.log(elements2);
 
   return (
-    <CytoscapeComponent elements={elements} style={{ width: '600px', height: '600px' }} />
-  )
-}
+    <CytoscapeComponent
+      elements={elements}
+      style={{
+        width: '600px',
+        height: '600px',
+      }}
+    />
+  );
+};
 
 export default HeapTree;
